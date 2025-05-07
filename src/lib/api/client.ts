3cd1a02d5649767b1e3f227c6eb2d3776b369fa5
Andrei <andrei.vupt@gmail.com>
2025-05-07 15:22:48 -0300

    feat: alter base url api railway

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -1,7 +1,17 @@
 import axios from 'axios'
 
+// API LOCAL
+
+// export const api = axios.create({
+//   baseURL: 'http://localhost:3030/',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// });
+
+// API DEPLOYED
 export const api = axios.create({
-  baseURL: 'http://localhost:3030/',
+  baseURL: 'https://scheduler-back-production-ccc4.up.railway.app/',
   headers: {
     'Content-Type': 'application/json',
   },
