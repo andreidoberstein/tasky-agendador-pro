cf1f796d9ef07719ad988ff02953e78798b6e97e
Andrei <andrei.vupt@gmail.com>
2025-05-25 13:43:56 -0300

    fix: set API from railway

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -2,17 +2,17 @@ import axios from 'axios'
 
 // API LOCAL
 
-export const api = axios.create({
-  baseURL: 'http://localhost:3030/',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
-
-// API DEPLOYED
 // export const api = axios.create({
-//   baseURL: 'https://scheduler-back-production-ccc4.up.railway.app/',
+//   baseURL: 'http://localhost:3030/',
 //   headers: {
 //     'Content-Type': 'application/json',
 //   },
 // });
+
+// API DEPLOYED
+export const api = axios.create({
+  baseURL: 'https://scheduler-back-production-ccc4.up.railway.app/',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
